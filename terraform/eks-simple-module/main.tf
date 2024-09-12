da511e551e406cc02c57b02b61d510d9f697dfa4
= <=>
2024-09-12 20:10:45 +0530

    added simple eks module from official aws tf

---
+++ terraform/eks-simple-module/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.21"
+  # subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+}
+
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "node_group_role_arn" {
+  value = module.eks.node_groups["eks_nodes"].iam_role_arn
+}
